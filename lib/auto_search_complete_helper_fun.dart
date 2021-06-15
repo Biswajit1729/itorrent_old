233add7884f54b60ffb4c85c43d0e6fcb72f5f37
Biswajit1729 <mandalbis1729@gmail.com>
2021-06-16 02:53:03 +0530

    auto search complete web bug fixd

--- lib/auto_search_complete_helper_fun.dart
+++ lib/auto_search_complete_helper_fun.dart
@@ -12,7 +12,12 @@ Future getSustions(String pattern) async {
   String queryString = Uri(queryParameters: queryParams).query;
   final requestUrl = endpointUrl + '?' + queryString;
 
-  var res = await http.get(Uri.encodeFull(requestUrl));
+  var res = await http.get(Uri.encodeFull(requestUrl), headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+    "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type,   Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  });
   List link = jsonDecode(res.body)[1];
   return link;
 }
